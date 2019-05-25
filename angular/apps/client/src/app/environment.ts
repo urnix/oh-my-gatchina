8e5ece6fb959a4e87a768f73e64dc69d5ef97417
Dmitriy <dmitriy.kolesov@gmail.com>
2019-05-25 17:00:13 +0300

    add firebase environment

---
+++ angular/apps/client/src/app/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  baseUrl: 'https://foodrazor-dmitriy.firebaseapp.com',
+  firebase: {
+    apiKey: 'AIzaSyD1n3R56zTJyrTue1wgTrzuLygALeNpEA8',
+    authDomain: 'oh-my-gatchina.firebaseapp.com',
+    databaseURL: 'https://oh-my-gatchina.firebaseio.com',
+    projectId: 'oh-my-gatchina',
+    storageBucket: 'oh-my-gatchina.appspot.com',
+    messagingSenderId: '56080447669',
+    appId: '1:56080447669:web:a27119bb7e640288'
+  }
+};
