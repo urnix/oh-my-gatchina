2562757a297140509727caad3b3180fb820e9ffd
Dmitriy <dmitriy.kolesov@gmail.com>
2019-05-26 14:47:01 +0300

    feed

--- angular/apps/client/src/app/feed/feed.component.ts
+++ angular/apps/client/src/app/feed/feed.component.ts
@@ -6,6 +6,7 @@ import { getEvents } from '../select-place/select-place.selectors';
 import { CityEvent } from '@shared/models/cityEvent.interface';
 import { getUserId } from '../+core/store/selectors';
 import { first } from 'rxjs/operators';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'angular-feed',
@@ -15,7 +16,7 @@ import { first } from 'rxjs/operators';
 export class FeedComponent implements OnInit {
   events$ = this.store.pipe(select(getEvents));
 
-  constructor(private store: Store<AppState>) {}
+  constructor(private store: Store<AppState>, private db: AngularFirestore) {}
 
   ngOnInit(): void {
     this.store.dispatch(new SelectPlaceLoadCollectionAction());
@@ -32,5 +33,19 @@ export class FeedComponent implements OnInit {
       return;
     }
     const users = [...event.users, userId];
+    await this.db.doc(`event/${event.id}`).update({ users });
+  }
+
+  async addUserDisabled(event: CityEvent) {
+    const userId = await this.store
+      .pipe(
+        select(getUserId),
+        first()
+      )
+      .toPromise();
+    if (event.users.indexOf(userId) >= 0) {
+      return true;
+    }
+    return false;
   }
 }
